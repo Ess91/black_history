86e5696dfb27c788630ec18d70a1f81fd812c6c0
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-15 21:50:33 +0000

    first commit

---
+++ lib/black_history.rb
@@ -0,0 +1,6 @@
+require "black_history/version"
+
+module BlackHistory
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/black_history/version.rb
@@ -0,0 +1,3 @@
+module BlackHistory
+  VERSION = "0.1.0"
+end
