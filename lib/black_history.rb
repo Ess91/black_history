01af2bf4e0a02b832ff6568120b1834739365ecc
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-15 22:54:33 +0000

    black_history.rb

--- lib/black_history.rb
+++ lib/black_history.rb
@@ -1,6 +1,8 @@
 require_relative "./black_history/version"
 require_relative "./black_history/cli"
 
+require 'pry'
+
 module BlackHistory
   class Error < StandardError; end
   # Your code goes here...
