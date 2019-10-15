b55f7e99f9a106236378fda6a1898cc9616e1ea5
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-15 23:37:41 +0000

    black_history

--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -1,7 +1,42 @@
 class BlackHistory::CLI
-  
   def call
-    puts "Welcome to the Black History App"
-    binding.pry 
+    puts "Welcome to the Black History App!"
+    months_available
+    months_event
+    user_month
+    
   end 
+  
+ # def events
+  #  @events = []
+  #end 
+  
+  def months_available
+    @months = ['September', 'October', 'November']
+  end 
+  
+  def months_event
+    puts "Choose month to see which event is happening!"
+    @months.each.with_index(1) do |month, index|
+      puts "#{index}. #{month}"
+    end 
+  end 
+  
+  #def list_events
+   # puts "Choose an event to see details."
+    #@events.each.with_index(1) do |event, index|
+     # puts "#{index}. #{event}"
+    #end 
+  #end 
+  
+  def user_month
+    month_chosen = gets.strip
+  end 
+  
+  def valid_input(input,data)
+    input <= data.length && input > 0 
+  end
+  
+    
 end
+
