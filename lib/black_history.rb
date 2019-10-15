ac69df0c87090e9f9a282d27f2abb1d39e097c49
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-15 22:51:56 +0000

    black_histoy

--- lib/black_history.rb
+++ lib/black_history.rb
@@ -1,4 +1,5 @@
-require "black_history/version"
+require_relative "./black_history/version"
+require_relative "./black_history/cli"
 
 module BlackHistory
   class Error < StandardError; end
--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -2,5 +2,6 @@ class BlackHistory::CLI
   
   def call
     puts "Welcome to the Black History App"
+    binding.pry 
   end 
 end
