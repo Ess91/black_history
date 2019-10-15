8b07173ad0e022c72c51924921859718d85592ce
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-15 22:35:34 +0000

    black_history

--- bin/blackhistory.rb
+++ bin/blackhistory.rb
@@ -1,1 +1,4 @@
 #!/usr/bin/env ruby
+
+require_relative '../lib/black_history'
+BlackHistory::CLI.new.call
---
+++ lib/black_history/cli.rb
@@ -0,0 +1,6 @@
+class BlackHistory::CLI
+  
+  def call
+    puts "Welcome to the Black History App"
+  end 
+end
