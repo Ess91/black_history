bc1a9a3338bd333f5958c0d23eb92a290acbaba8
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-16 16:20:52 +0000

    black_history

--- bin/blackhistory.rb
+++ bin/blackhistory.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/black_history'
+
 BlackHistory::CLI.new.call
--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -1,60 +1,21 @@
 class BlackHistory::CLI
   def call
     puts "Welcome to the Black History App!"
-    display_events
+    locations
+    user_months
     menu
     goodbye
-    
   end 
   
-  def display_events
-    puts "Choose event that you are interested in!"
-    @events.each.with_index(1) do |event, index|
-      puts "#{index}. #{event}"
-    end 
-  
-  #def months_available
-   # @months = BlackHistory::Month.all 
-    #binding.pry 
-  #end 
-  
-  def events
-    @events = []
-  end 
-  
- # def months_available
-   # @months = ['September', 'October', 'November']
-    #binding.pry
-  #end 
-  
-  #def months_event
-   # puts "Choose month to see which event is happening!"
-    #@months.each.with_index(1) do |month, index|
-     # puts "#{index}. #{month}"
-    #end 
-  #end 
-  
-  #def list_events
-   # puts "Choose an event to see details."
-    #@events.each.with_index(1) do |event, index|
-     # puts "#{index}. #{event}"
-    #end 
-  #end 
-  
-  def user_month
-    month_chosen = gets.strip.to_i
-    show_events(month_chosen) if valid_input(month_chosen.to_i, @months)
-  end 
-  
-  def valid_input(input,data)
-    input.to_i <= data.length && input > 0 
-  end
-  
-  def show_events(month_chosen)
-    month = @months[month_chosen -1]
-    puts "These are the events for #{month}"
-    #binding.pry
-  end
-end 
-end 
+  def locations 
+    #scrapped later 
+    @location = ["Camden", "Islington", "Hackney", "Southwark", "Haringey"]
+end
 
+def user_month 
+  @months = ['']
+end 
+end
+
+    
+  
