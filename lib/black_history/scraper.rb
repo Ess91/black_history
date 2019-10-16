9a9dcb70c07dd4fb3a527ec42589bc495dc94037
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-16 21:31:51 +0000

    black_history

--- lib/black_history/scraper.rb
+++ lib/black_history/scraper.rb
@@ -1,6 +1,16 @@
 class BlackHistory::Scraper
   def self.scrape_locations
     doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/listings/region/greater-london/"))
-    binding.pry 
-end jj 
+   locations = doc.css("span.post-listings-list-tax").text
+   dates = doc.css("span.post-listings-list-date").text 
+   events = doc.css("h2.post-listings-list-title").text
+   
 end
+
+  events = doc.css(".post-listings-list-title")
+   events_url = events.css("a").attr("href").value
+   
+end
+
+doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/article/listings/region/city-of-london/sunday-sorbet-caribbean-bruch/"))
+    descriptions = doc.css(".structured-content").first.text
