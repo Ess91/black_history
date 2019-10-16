74162b8d4a9a23578af3d050b38b3407b7179ee6
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-16 17:58:09 +0000

    black_history

--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -26,7 +26,7 @@ def user_locations
 
 def get_user_locations
   chosen_location = gets.strip
-  if valid_input (chosen_location, @locations)
+  if valid_input(chosen_location, @locations)
     show_events(chosen_location)
   end
 end 
@@ -36,6 +36,7 @@ def valid_input (input, data)
 end
 
 def show_events(chosen_location)
+  location = @locations[chosen_location -1]
   puts "Here are a list of events for #{location} chosen"
   binding.pry 
 end
