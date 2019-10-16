6a6e4464608d7e0d0fe6841abf77605e7e48e697
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-16 16:58:27 +0000

    black_history

--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -1,35 +1,36 @@
 class BlackHistory::CLI
   def call
     puts "Welcome to the Black History App!"
- #   locations
-  #  user_locations
-   # menu
+    locations
+    user_locations
+   #menu
     #goodbye
-    get_months
-    list_months
+    #get_months
+    #list_months
   end 
   
-  #def locations 
+  def locations 
     #scrapped later 
-   # @locations = ["Camden", "Islington", "Hackney", "Southwark", "Haringey"]
-#end
+    @locations = ["Camden", "Islington", "Hackney", "Southwark", "Haringey"]
+end
 
-def get_months
-  @months = ["January", "Feburary", "March"]
-end 
 
-def list_months
-  @months.each.with_index(1) do |month, index|
-    puts "#{index}. #{month}"
-  end 
-end 
-end 
 
-#def user_locations 
+def user_locations 
   #list locations 
- # @locations.each.with_index(1) do |location, index|
-  #puts "#{index}. #{location}"
-  #end 
- #end 
+  @locations.each.with_index(1) do |location, index|
+    puts "#{index}. #{location}"
+  end 
+ end 
+end 
+
+#def get_months
+ # @months = ["January", "Feburary", "March"]
 #end 
 
+#def list_months
+ # @months.each.with_index(1) do |month, index|
+  #  puts "#{index}. #{month}"
+  #end 
+#end 
+#end 
