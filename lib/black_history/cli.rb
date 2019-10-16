212722ada254fee2e514a750557c52dabb58b492
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-16 16:50:45 +0000

    black_history

--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -1,21 +1,35 @@
 class BlackHistory::CLI
   def call
     puts "Welcome to the Black History App!"
-    locations
-    user_months
-    menu
-    goodbye
+ #   locations
+  #  user_locations
+   # menu
+    #goodbye
+    get_months
+    list_months
   end 
   
-  def locations 
+  #def locations 
     #scrapped later 
-    @location = ["Camden", "Islington", "Hackney", "Southwark", "Haringey"]
-end
+   # @locations = ["Camden", "Islington", "Hackney", "Southwark", "Haringey"]
+#end
 
-def user_month 
-  @months = ['']
+def get_months
+  @months = ["January", "Feburary", "March"]
 end 
-end
 
-    
-  
+def list_months
+  @months.each.with_index(1) do |month, index|
+    puts "#{index}. #{month}"
+  end 
+end 
+end 
+
+#def user_locations 
+  #list locations 
+ # @locations.each.with_index(1) do |location, index|
+  #puts "#{index}. #{location}"
+  #end 
+ #end 
+#end 
+
--- lib/black_history/scraper.rb
+++
@@ -1,5 +0,0 @@
-class BlackHistory::Scraper
-  def self.scrape_months
-    doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/listings/region/greater-london/"))
-end
-end
