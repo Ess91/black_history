b3668d0f0e0dcfd0aa64125084d3dbae2b3ec987
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-20 21:31:28 +0000

    black_history

--- NOTES.md
+++ NOTES.md
@@ -23,3 +23,38 @@ doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/article/listings
     
   #end 
   
+ # def print_location(input)
+  #  puts ""
+   # puts "---------- Locations----------"
+    #puts ""
+    # Oct_BlackHistory.all.each.with_index(1) do |location, index|
+    #  puts "#{index}. #{location.locations}"
+    #end
+  #end
+  
+  #  def print_restaurant(restaurant)
+ #   puts ""
+  #  puts "----------- #{location.locations}"
+   # puts ""
+    #puts "Events:           #{location.events}"
+    #puts "Dates:            #{location.dates}"
+    #puts "Event URL:        #{location.event_url}"
+    #puts "Description:      #{location.descriptions}"
+    #puts ""
+ 
+   puts ""
+    puts "Would you like to see another restaurant? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts ""
+      puts "Thank you! Have a great day!"
+      exit
+    else
+      puts ""
+      puts "I don't understand that answer."
+      start
+    end
+  end
--- lib/black_history/cli.rb
+++ lib/black_history/cli.rb
@@ -1,40 +1,40 @@
 class BlackHistory::CLI
 
 
-  def call
-    welcome
-    display_locations
-    user_input
-    find_events("Greenwich")
+ # def call
+  #  welcome
+   # display_locations
+    #user_input
+    #find_events("Greenwich")
     #display_dates
     #display_times
     #display_descriptions
     #goodbye
-  end 
+  #end 
   
-  def welcome 
-    puts "Welcome to Black History London"
-  end 
+ # def welcome 
+  #  puts "Welcome to Black History London"
+  #end 
   
-def display_locations
-    Oct_BlackHistory.all.each.with_index do |location, index|
+#def display_locations
+ #   Oct_BlackHistory.all.each.with_index(1) do |location, index|
      # binding.pry
-      puts "#{index + 1}. #{location.locations}"
+  #    puts "#{index}. #{location.locations}"
     #  binding.pry 
-    end
+   # end
   #  binding.pry
-  end
-end 
+  #end
+#end 
   
  # def user_prompt
   #  puts "Select event"
   #end 
   
-  def user_input
-     value = nil
-     value = gets.chomp
-     index = value.to_i - 1
-   end
+ # def user_input
+  #   value = nil
+   #  value = gets.chomp
+    # index = value.to_i - 1
+  # end
 
 #def valid_response(input)
  # max_value = Oct_BlackHistory.all.length
@@ -45,30 +45,79 @@ end
   #  input.to_i <= data.length && input.to_i > 0
   #end
 
-def find_events(location)
- Oct_BlackHistory.all.select do |event|
-   location == events.locations
+#def find_events(location)
+ #Oct_BlackHistory.all.select do |event|
+  # location == events.locations
   # binding.pry
   # events.events.each_with_index do |data, index|
-   end
-   puts "#{events}"
- end
+   #end
+   #puts "#{events}"
+# end
  
  
-   def show_events_for(chosen_location)
-    location = Oct_BlackHistory[chosen_location - 1]
-    location.get_events
-    puts "Here are events for #{location.locations}"
-    location.events.each.with_index(1) do |event, index|
-      puts "#{index}. #{location.events}"
-    end
-    get_user_event(month)
-  end
+ #  def show_events_for(chosen_location)
+  #  location = Oct_BlackHistory[chosen_location - 1]
+   # location.get_events
+    #puts "Here are events for #{location.locations}"
+    #location.events.each.with_index(1) do |event, index|
+     # puts "#{index}. #{location.events}"
+    #end
+    #get_user_event(month)
+  #end
   
-  def get_user_event(month)
-    puts "Choose an event to see more details."
-    input = gets.strip
-    event = location.events[input.to_i - 1]
-    event.get_event_details
-    show_event_details(event)
+#  def get_user_event(month)
+ #   puts "Choose an event to see more details."
+  #  input = gets.strip
+   # event = location.events[input.to_i - 1]
+    #event.get_event_details
+    #show_event_details(event)
+  #end
+  
+  
+  def call 
+    welcome
+    start
+    find_events
+  end 
+  
+    def welcome 
+    puts "Welcome to Black History London"
+    start 
+  end 
+  
+
+  def start
+    puts ""
+    puts "Please select the location you wish to see:"
+    Oct_BlackHistory.all.each.with_index (1) do |location, index|
+      puts "#{index}. #{location.locations}"
+    #input = gets.strip.to_i
+  end 
+end 
+
+ def find_events
+    Oct_BlackHistory.each.with_index do |events, i|
+      index == i
   end
+puts "#{events}"
+end
+end 
+
+   # def display_events
+   #   Oct_BlackHistory.each_with_index(1) do |events, index|
+    #    puts "#{index}. #{location.events}"
+     # end
+    #end 
+  #end 
+      
+    #print_location(input)
+
+  #  puts ""
+   # puts "What location would you like more information on?"
+    #input = gets.strip
+
+    #events = Oct_BlackHistory.find(input.to_i)
+
+    #print_restaurant(events)
+
+  
