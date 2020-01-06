d9fab26c79a092e08e2d0940a707be34d7888622
Esther Agyare-Boateng <eagyare91@gmail.com>
2020-01-06 14:50:39 +0000

    black_history

--- lib/black_history/scraper.rb
+++ lib/black_history/scraper.rb
@@ -1,23 +1,30 @@
 class BlackHistory::Scraper
   
   def self.scrape_locations
-    doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/listings/region/greater-london/"))
-    doc.css(".post-list-item--article").each do |page| 
+    page = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/listings/region/greater-london/"))
+    page.css(".post-list-item--article").each do |page| #list-item - The article column that contains date, event, description
    location = BlackHistory::Oct_BlackHistory.new
    location.locations = page.css("span.post-listings-list-tax").text
    location.dates = page.css("span.post-listings-list-date").text 
    location.events = page.css("h2.post-listings-list-title").text
-   location.event_url = page.css("a").attr("href").value
+   location.event_url = page.css("a").attr("href").value 
   end 
 end 
+end
   
  
 #  doc = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/article/listings/region/city-of-london/sunday-sorbet-caribbean-bruch/"))
+
+
   
+ # def self.second_scrape
+ #def event_url
+  #  page = Nokogiri::HTML(open("https://www.blackhistorymonth.org.uk/article/listings/region/lambeth/gamelan-poetry-workshops-lambeth-primary-offer-2019-2020/"))
+    
  # def event_url
-  #  location.descriptions = doc.css(".content-main p").map do |description|
-   # description.text
+   # location.descriptions = doc.css(".content-main p").map do |description|
+    #description.text
     #end 
     #descriptions.join("\n")
-  end 
-  
+  #end 
+#end 
